16154bbbadd801b9d3557b867367ee7df0935d48
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-06-12 23:34:02 +0100

    Replace moduleutils with dynamicpy

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# amethyst
+# Amethyst
 
 An addon for [discord.py](https://github.com/Rapptz/discord.py) to add utility methods used by my discord bots.
--- src/amethyst/client.py
+++ src/amethyst/client.py
@@ -1,11 +1,12 @@
-from amethyst import moduleutils
-from types import ModuleType
 from amethyst import errors
 from typing import Any
+import dynamicpy
 import discord
 import logging
 
 __all__ = ("AmethystClient",)
+
+_default_modules = [".commands"]
 
 _log = logging.getLogger(__name__)
 
@@ -14,40 +15,43 @@ class AmethystClient(discord.Client):
     def __init__(
         self,
         intents: discord.Intents,
+        *,
+        search_modules: list[str] | None = None,
         **options: Any,
     ) -> None:
         self._home_package = self._get_home_package()
+        self._load_modules(search_modules or _default_modules)
 
         super().__init__(intents=intents, **options)
 
     @property
-    def home_package(self) -> ModuleType | None:
+    def home_package(self) -> str | None:
         """The parent package of the module where this client was instantiated. Is `None` if it was a top-level module."""
         return self._home_package
 
-    def _locate_module(self, path: str) -> ModuleType:
-        if self.home_package is None and path.startswith("."):
-            path = path[1:]
+    def _load_modules(self, search_modules: list[str]) -> None:
+        loader = dynamicpy.DynamicLoader()
+        loader.register_handler(lambda n, v: print(n))
 
+        for module in search_modules:
+            if self.home_package is None and module.startswith("."):
+                module = module[1:]
+            loader.load_module(module, self.home_package)
+
+    def _get_home_package(self) -> str | None:
         try:
-            return moduleutils.get_module(path, self.home_package)
-        except ImportError:
-            raise errors.ModuleLocateError("Could locate module '%s'", path)
+            module = dynamicpy.get_foreign_module()
 
-    def _get_home_package(self) -> ModuleType | None:
-        try:
-            module = moduleutils.get_caller_module()
-
-            if moduleutils.is_package(module):
+            if dynamicpy.is_package(module):
                 package = module
             else:
-                package = moduleutils.get_parent(module)
+                package = dynamicpy.get_module_parent(module)
 
-            _log.debug("Home package located as '%s'", package.__name__)
+            _log.debug("Home package located as '%s'", package)
             return package
-        except moduleutils.NoParentError:
+        except dynamicpy.NoParentError:
             _log.debug("Instantiating module is top-level.")
             return None
-        except (ImportError, moduleutils.ModuleUtilsError) as e:
+        except ImportError as e:
             _log.error("Error locating home package: %s", e)
             raise errors.ModuleLocateError("Error locating home package") from e
