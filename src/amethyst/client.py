dbbaf9b35356b48a68442d8c57bbe7168c987452
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-06-25 17:54:00 +0100

    added AmethystClient.commands_in_sync

--- src/amethyst/client.py
+++ src/amethyst/client.py
@@ -4,8 +4,10 @@ from typing import Any, Self
 import discord
 import dynamicpy
 from discord import app_commands
+from discord.abc import Snowflake
 
 from amethyst import errors
+from amethyst.utils import is_dict_subset
 from amethyst.widget import AmethystCommand
 
 __all__ = ("AmethystClient",)
@@ -51,23 +53,54 @@ class AmethystClient(discord.Client):
         _log.debug("Registering command '%s'", command.name)
         self._tree.add_command(command)
 
+    async def commands_in_sync(self, guild: Snowflake | None = None) -> bool:
+        """Checks if the the commands locally registered to this client are in sync with the commands on discord.
+
+        Parameters
+        ----------
+        guild : Snowflake, optional
+            The guild to check commands from. If left blank, checks global commands.
+
+        Returns
+        -------
+        bool
+            Returns true if the local commands match the remote commands.
+        """
+        local = {c.name: c.to_dict() for c in self.tree.get_commands(guild=guild)}
+        remote = {
+            c.name: {
+                "nsfw": c.nsfw,
+                "dm_permission": c.dm_permission,
+                "default_member_permissions": c.default_member_permissions,
+                **c.to_dict(),
+            }
+            for c in await self.tree.fetch_commands(guild=guild)
+        }
+
+        # Check for commands that only appear in one location
+        if len(set(local.keys()).symmetric_difference(remote.keys())) > 0:
+            return False
+
+        # Check for changes to existing commands
+        return is_dict_subset(remote, local)
+
     def _build_loader(self) -> dynamicpy.DynamicLoader:
+        """Builds a DynamicLoader for registering widgets to the client."""
         loader = dynamicpy.DynamicLoader()
 
-        loader.register_handler(
-            lambda _, v: self.register_command(v),  # type: ignore
-            lambda _, v: isinstance(v, AmethystCommand),
-        )
+        loader.register_type_handler(lambda _, v: self.register_command(v), AmethystCommand)
 
         return loader
 
     def _load_modules(self, search_modules: list[str]) -> None:
+        """Loads the modules specified in `search_modules` using the client's DynamicLoader."""
         for module in search_modules:
             if self.home_package is None and module.startswith("."):
                 module = module[1:]
             self._loader.load_module(module, self.home_package)
 
     def _get_home_package(self) -> str | None:
+        """Return the home package of the application."""
         try:
             module = dynamicpy.get_foreign_module()
 
